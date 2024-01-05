4fb2a13ea91f3a74dea6168efe95ecd4471a32c2
dante <dellatogna.io@gmail.com>
2024-01-05 14:13:42 -0500

    test2

--- test2.py
+++ test2.py
@@ -9,39 +9,91 @@ colors = {
     "blue": (0, 0, 255),
 }
 
+bodies = list()
+
 
 class Body:
-    def __init__(self, color, mass, pos, speed, gravity):
+    def __init__(self, color, mass, pos, speed, past_speed, gravity):
         self.color = color
         self.mass = mass
         self.pos = pos
         self.speed = speed
+        self.past_speed = past_speed
         self.gravity = gravity
+
+        bodies.append(self)
 
 
 class Ball(Body):
-    def __init__(self, color, mass, pos, speed, gravity, radius):
-        super().__init__(color, mass, pos, speed, gravity)
+    def __init__(self, color, mass, pos, speed, past_speed, gravity, radius):
+        super().__init__(color, mass, pos, speed, past_speed, gravity)
         self.radius = radius
 
 
-width = 800
-height = 800
+def main_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-g = 0.00000981
+        # gravity
+        time = pygame.time.get_ticks()
+        for body in bodies:
+            if body.gravity == True:
+                body.speed[1] = g * time + body.past_speed[1]
+                body.past_speed[1] = body.speed[1]
 
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
+                # pygame.time.delay()
 
-spawn = [100, 700]
-ball = Ball(color=colors["red"], mass=1, pos=spawn, speed=0, gravity=True, radius=40)
+                print(body.speed[1])
+                print(body.pos)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+                body.pos[1] = body.pos[1] + time * body.speed[1]
 
-    pygame.display.update()
-    clock.tick(1000)
+        screen.fill("black")
+
+        for body in bodies:
+            pygame.draw.circle(
+                surface=screen,
+                color=body.color,
+                center=body.pos,
+                radius=body.radius,
+            )
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    width = 800
+    height = 800
+
+    g = 0.00000981
+
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    clock = pygame.time.Clock()
+
+    spawn = [100, 100]
+    ball1 = Ball(
+        color=colors["white"],
+        mass=1,
+        pos=spawn,
+        speed=[0, 0],
+        past_speed=[0, 0],
+        gravity=True,
+        radius=40,
+    )
+
+    ball2 = Ball(
+        color=colors["red"],
+        mass=2,
+        pos=[600, 300],
+        speed=[0, 0],
+        past_speed=[0, 0],
+        gravity=True,
+        radius=80,
+    )
+
+    main_loop()
