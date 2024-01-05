358355a3f7d13a4c53e275c848c05094c4e8f025
dante <dellatogna.io@gmail.com>
2024-01-05 17:03:19 -0500

    begun working on the solar_system

---
+++ solar_system.py
@@ -0,0 +1,21 @@
+import pygame
+import numpy
+import sys
+
+
+class Bodies:
+    def __init__(self, color, mass, radius, pos, fx, fy):
+        self.color = color
+        self.mass = mass
+        self.radius = radius
+        self.pos = pos
+        self.fx = fx
+        self.fy = fy
+
+
+def main_loop():
+    pass
+
+
+if __name__ == "__main__":
+    main_loop()
