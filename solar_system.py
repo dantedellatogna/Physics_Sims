46fe929313e775f8bfe11a9dbdd6515976dd1925
dante <dellatogna.io@gmail.com>
2024-01-13 03:38:21 -0500

    Code is clearer now

--- solar_system.py
+++ solar_system.py
@@ -2,7 +2,7 @@ import pygame
 import math
 import sys
 
-
+# ---COLOR PALETTE (RGB)---
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -10,86 +10,73 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-width = 1900
-height = 900
+# --- WINDOW SIZE ---
+WIDTH = 1900
+HEIGHT = 900
 
-G = 6.674e-11
-solar_mass = 1.988e30
-TIMESTEP = 24 * 3600  # 1 day
-AU = 149.6e6 * 1000
+# --- PHYSICS SIMULATION CONSTANTS ---
+G = 6.674e-11  # Gravitational constant
+TIME = 24 * 3600  # 1 day in seconds
+AU = 149.6e6 * 1000  # Astronomical Unit (meters)
 SCALE = 200 / AU  # 1AU = 100px
 
-
+# --- ASTRONOMICAL BODIES SIMULATED ---
 bodies = list()
 
 
 class Bodies:
-    def __init__(self, color, pos, mass, radius, speed, orbit_trail):
-        self.color = color
-        self.pos = pos
-        self.mass = mass
-        self.radius = radius
-
-        self.speed = speed
-
-        self.orbit_trail = orbit_trail
+    def __init__(self, color, position, mass, radius, speed, orbit_trail):
+        self.color = color  # tuple RGB
+        self.position = position  # list [x, y]
+        self.mass = mass  # kg
+        self.radius = radius  # px
+        self.speed = speed  # m/s
+        self.orbit_trail = orbit_trail  # list of positions [[x1, y1], [x2, y2],...]
 
         bodies.append(self)
 
     def draw_body(self):
-        x = self.pos[0] * SCALE + width / 2
-        y = self.pos[1] * SCALE + height / 2
-        print("x, y: ", [x, y])
+        x = self.position[0] * SCALE + WIDTH / 2
+        y = self.position[1] * SCALE + HEIGHT / 2
+
+        # Drawing orbit lines
+        self.orbit_trail.append([x, y])
+        prev_orbit = self.orbit_trail[0]
+
+        for orbit in self.orbit_trail:
+            pygame.draw.line(
+                surface=screen,
+                color=WHITE,
+                start_pos=prev_orbit,
+                end_pos=orbit,
+                width=1,
+            )
+            prev_orbit = orbit
+
+        # Drawing bodies
         pygame.draw.circle(
             surface=screen, color=self.color, center=[x, y], radius=self.radius
         )
 
-    def draw_orbit(self):
-        for body in bodies[1:]:
-            x = body.pos[0] * SCALE + width / 2
-            y = body.pos[1] * SCALE + height / 2
-            body.orbit_trail.append([x, y])
-            prev_orbit = body.orbit_trail[0]
-
-            for orbit in body.orbit_trail:
-                pygame.draw.line(
-                    surface=screen,
-                    color=body.color,
-                    start_pos=prev_orbit,
-                    end_pos=orbit,
-                )
-                prev_orbit = orbit
-
     def attraction(self, other):
         if self != bodies[0]:
-            x_distance = other.pos[0] - self.pos[0]
-            y_distance = other.pos[1] - self.pos[1]
+            x_distance = other.position[0] - self.position[0]
+            y_distance = other.position[1] - self.position[1]
 
             r = math.sqrt(x_distance**2 + y_distance**2)
-
             F = G * self.mass * other.mass / r**2
 
             angle = math.atan2(y_distance, x_distance)
-
             Fx = F * math.cos(angle)
             Fy = F * math.sin(angle)
 
-            # print("Fx", Fx)
-            # print("Fy", Fy)
-
             past_speed = self.speed
+            self.speed[0] = (Fx * TIME) / self.mass + past_speed[0]
+            self.speed[1] = (Fy * TIME) / self.mass + past_speed[1]
 
-            time = TIMESTEP
-            # time = pygame.time.get_ticks()
-            self.speed[0] = (Fx * time) / self.mass + past_speed[0]
-
-            self.speed[1] = (Fy * time) / self.mass + past_speed[1]
-
-    def update_pos(self):
-        time = TIMESTEP
-        # time = pygame.time.get_ticks()
-        self.pos[0] = self.pos[0] + time * self.speed[0]
-        self.pos[1] = self.pos[1] + time * self.speed[1]
+    def update_position(self):
+        self.position[0] = self.position[0] + TIME * self.speed[0]
+        self.position[1] = self.position[1] + TIME * self.speed[1]
 
 
 def main_loop():
@@ -103,21 +90,18 @@ def main_loop():
 
         for body in bodies:
             body.attraction(bodies[0])
-            body.update_pos()
-            body.draw_orbit()
+            body.update_position()
             body.draw_body()
-            print("Color: ", body.color)
-            print("Position: ", body.pos)
-            print("Speed: ", body.speed)
 
         pygame.display.update()
-        clock.tick(1000)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
+    # Initializing bodies
     sun = Bodies(
         color=YELLOW,
-        pos=[0, 0],
+        position=[0, 0],
         mass=1.988e30,
         radius=50,
         speed=[0, 0],
@@ -126,8 +110,7 @@ if __name__ == "__main__":
 
     earth = Bodies(
         color=BLUE,
-        # pos=[0, 400],
-        pos=[1 * AU, 0],
+        position=[1 * AU, 0],
         mass=5.972e24,
         radius=12,
         # speed=[0, -40000],
@@ -137,7 +120,7 @@ if __name__ == "__main__":
 
     mars = Bodies(
         color=RED,
-        pos=[1.524 * AU, 0],
+        position=[1.524 * AU, 0],
         mass=6.39e23,
         radius=10,
         speed=[0, -24.077 * 1000],
@@ -146,14 +129,15 @@ if __name__ == "__main__":
 
     mercury = Bodies(
         color=GREEN,
-        pos=[0.387 * AU, 0],
+        position=[0.387 * AU, 0],
         mass=3.30e23,
         radius=8,
         speed=[0, -47.4 * 1000],
         orbit_trail=[],
     )
 
+    # Pygame initializations
     pygame.init()
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     main_loop()
